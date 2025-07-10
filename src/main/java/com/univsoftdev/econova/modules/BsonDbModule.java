9c0128c6b1b39fe2f505f48bec99e67522a84b5a
yasmramos <yasmramos95@gmail.com>
2025-07-10 15:53:23 +0200

    refactor(modulo): Simplificar la creación de BsonDb eliminando la clase Provider innecesaria

--- src/main/java/com/univsoftdev/econova/modules/BsonDbModule.java
+++ src/main/java/com/univsoftdev/econova/modules/BsonDbModule.java
@@ -1,22 +1,18 @@
 package com.univsoftdev.econova.modules;
 
-import jakarta.inject.Singleton;
 import com.univsoftdev.econova.component.wizard.BsonDb;
+import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
-import jakarta.inject.Provider;
+import jakarta.inject.Singleton;
 
 @Factory
 public class BsonDbModule {
 
+    @Bean
     @Singleton
-    public static class BsonDbProvider implements Provider<BsonDb> {
-
-        @Override
-        public BsonDb get() {
-            BsonDb db = new BsonDb();
-            return db;
-        }
-
+    public BsonDb bsonDb() {
+        BsonDb db = new BsonDb();
+        return db;
     }
 
 }
