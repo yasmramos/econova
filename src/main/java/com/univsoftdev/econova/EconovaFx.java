df964cdf975c75099757f921b00bfb12b494c8b8
yasmramos <yasmramos95@gmail.com>
2025-08-01 17:29:07 +0200

    feat: agregar anotaciones @NotNull para mejorar la validación de parámetros en EconovaFx

--- src/main/java/com/univsoftdev/econova/EconovaFx.java
+++ src/main/java/com/univsoftdev/econova/EconovaFx.java
@@ -1,5 +1,6 @@
 package com.univsoftdev.econova;
 
+import jakarta.validation.constraints.NotNull;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -9,7 +10,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.jetbrains.annotations.NotNull;
 
 public class EconovaFx extends Application {
 
@@ -23,16 +23,16 @@ public class EconovaFx extends Application {
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    static void setRoot(@NotNull String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static String loadCSS(String css) {
+    private static String loadCSS(@NotNull String css) {
         return Objects.requireNonNull(EconovaFx.class.getClassLoader().getResource(css + ".css")).toExternalForm();
     }
     
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(EconovaFx.class.getClassLoader().getResource(fxml + ".fxml"));
+    private static Parent loadFXML(@NotNull String fxml) throws IOException {
+        final var fxmlLoader = new FXMLLoader(EconovaFx.class.getClassLoader().getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
