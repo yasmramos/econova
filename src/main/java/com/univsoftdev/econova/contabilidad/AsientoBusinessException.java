73d5c15407a8178e5bc0c8baf444c91b8354a4ea
yasmramos <yasmramos95@gmail.com>
2025-08-01 17:36:16 +0200

    feat: agregar constructores adicionales a AsientoBusinessException para mejorar la gestión de excepciones

--- src/main/java/com/univsoftdev/econova/contabilidad/AsientoBusinessException.java
+++ src/main/java/com/univsoftdev/econova/contabilidad/AsientoBusinessException.java
@@ -5,5 +5,20 @@ public class AsientoBusinessException extends RuntimeException {
     public AsientoBusinessException(String message) {
         super(message);
     }
-}
 
+    public AsientoBusinessException() {
+    }
+
+    public AsientoBusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AsientoBusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    public AsientoBusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
